6da7fca9ce9a2e530ad4d7de3b1d7ec50a0bb766
Bao Phan <bao_d_phan@homedepot.com>
2019-08-16 11:02:13 -0500

    add config from package.json

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,17 @@
-// import config from "./config.json";
+const config = require("./config.json");
+const pkg = require("../package.json");
 
-module.exports = { REACT_APP_CONFIG: require("./config.json") };
+const initCap = s =>
+  s
+    .split("-")
+    .map(el =>
+      typeof el !== "string" ? el : el.charAt(0).toUpperCase() + el.slice(1)
+    )
+    .join(" ");
+
+function getConfig() {
+  let configs = Object.assign({}, config, { title: initCap(pkg.name) });
+  return configs;
+}
+
+module.exports = { REACT_APP_CONFIG: getConfig() };
